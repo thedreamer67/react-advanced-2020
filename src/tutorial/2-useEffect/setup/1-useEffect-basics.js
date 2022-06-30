189d49eb9974978e97beeb12d7715a5e2a990a3d
thedreamer67 <shaunaliting@gmail.com>
2022-06-30 15:46:50 +0800

    useEffect 1st eg

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
+// react hooks CANNOT be called in conditionals (i.e. if/else blocks) --> put the conditional IN the hook body itself :)
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+	const [value, setValue] = useState(0);
+
+	useEffect(() => {
+		console.log('call useEffect');
+		if (value > 0) {
+			document.title = `New messages(${value})`;
+		}
+	});
+
+	console.log('render component');
+
+	return (
+		<>
+			<h1>{value}</h1>
+			<button className='btn' onClick={() => setValue(value + 1)}>
+				click me
+			</button>
+		</>
+	);
 };
 
 export default UseEffectBasics;
