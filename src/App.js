ce884940280bad526c355e2f7bc7e853974e8889
thedreamer67 <shaunaliting@gmail.com>
2022-07-03 15:05:49 +0800

    finished conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns';
+import Setup from './tutorial/3-conditional-rendering/setup/3-show-hide';
 
 function App() {
-	return (
-		<div className='container'>
-			<Setup />
-		</div>
-	);
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  );
 }
 
 export default App;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,32 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world'; //returns the first truthy value encountered
+  // const secondValue = text && 'hello world'; //returns '' if entire expression is falsy, else return the last value
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      {/* returns text if not empty, else return 'john doe' */}
+      <h1>{text || 'john doe'}</h1>
+      <button
+        className='btn'
+        onClick={() => {
+          setIsError((prevError) => {
+            return !prevError;
+          });
+        }}>
+        toggle error
+      </button>
+      {/*//! returns <h1>hello world</h1> if text is not empty, else return nothing */}
+      {text && <h1>hello world</h1>}
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>there is an error</p> : <p>there is no error</p>}
+    </>
+  );
 };
 
 export default ShortCircuit;
